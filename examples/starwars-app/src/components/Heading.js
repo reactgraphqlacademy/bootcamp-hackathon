e284d1fa683cbe765062008501b4e9ca9742ab25
Horacio Herrera <hherrerag@me.com>
2018-08-13 00:00:38 +0200

    add star wars logo to heading

--- examples/starwars-app/src/components/Heading.js
+++ examples/starwars-app/src/components/Heading.js
@@ -1,8 +1,14 @@
 import React from "react";
 import { Heading as BaseHeading } from "rebass";
+import { Logo } from "./index";
 
 function Heading(props) {
-  return <BaseHeading {...props}>{props.children}</BaseHeading>;
+  return (
+    <div>
+      <Logo width={200} />
+      <BaseHeading {...props}>{props.children}</BaseHeading>
+    </div>
+  );
 }
 
 export default Heading;
--- examples/starwars-app/src/components/index.js
+++ examples/starwars-app/src/components/index.js
@@ -2,3 +2,4 @@ export { default as PeopleList } from "./PeopleList";
 export { default as Container } from "./Container";
 export { default as Heading } from "./Heading";
 export { default as Loading } from "./Loading";
+export { default as Logo } from "./Logo";
